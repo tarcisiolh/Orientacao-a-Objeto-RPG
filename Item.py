fb06b1e4e9edd11e4631b066cb4b3b25b2b46ae6
Tarcísio Luiz Harres de Almeida <160616012+tarcisiolh@users.noreply.github.com>
2026-05-07 09:05:39 -0300

    encapsulamento de item (tinha esquecido)

--- Item.py
+++ Item.py
@@ -2,10 +2,23 @@ from TipoItem import TipoItem
 
 class Item:
     def __init__(self, nome: str, descricao: str, tipo: TipoItem, valorEfeito: float):
-        self.nome = nome 
-        self.descricao = descricao 
-        self.tipo = tipo 
-        self.valorEfeito = valorEfeito
+        self.__nome = nome 
+        self.__descricao = descricao 
+        self.__tipo = tipo 
+        self.__valorEfeito = valorEfeito
 
+    @property
+    def nome(self):
+        return self.__nome
+    @property
+    def descricao(self):
+        return self.__descricao
+    @property
+    def tipo(self):
+        return self.__tipo
+    @property
+    def valorEfeito(self):
+        return self.__valorEfeito
+    
     def __str__(self):
-        return f"{self.nome} [{self.tipo.value}] - Efeito: {self.valorEfeito}"
+        return f"{self.__nome} [{self.__tipo.value}] - Efeito: {self.__valorEfeito}"
